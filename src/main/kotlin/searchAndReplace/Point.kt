b8afbc43a367ff2dd33b1ea85bd6b139d1de14d5
Wout Vinckevleugel <50151315+Wovi10@users.noreply.github.com>
2022-10-29 14:43:48 +1100

    Update Point.kt

--- src/main/kotlin/searchAndReplace/Point.kt
+++ src/main/kotlin/searchAndReplace/Point.kt
@@ -4,13 +4,13 @@ package searchAndReplace
  * This class was used to try structural search and replace in Kotlin
  */
 class Point constructor(locationX: Int, locationY: Int) {
-    val x: Int  = locationX
-    val y: Int  = locationY
+    val x: Int = locationX
+    val y: Int = locationY
 }
 
-open class OpenClass{
+open class OpenClass {
     open val openProperty = 0
-    open fun display(){
+    open fun display() {
         println("Some text")
         print("More text")
         print(0)

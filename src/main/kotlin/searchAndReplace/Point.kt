ac30fbe927a3a74372c15688223085105f15e2bd
Wout Vinckevleugel <50151315+Wovi10@users.noreply.github.com>
2022-10-29 14:37:48 +1100

    Update Point.kt

--- src/main/kotlin/searchAndReplace/Point.kt
+++ src/main/kotlin/searchAndReplace/Point.kt
@@ -4,3 +4,12 @@ class Point constructor(locationX: Int, locationY: Int) {
     val x: Int  = locationX
     val y: Int  = locationY
 }
+
+open class OpenClass{
+    open val openProperty = 0
+    open fun display(){
+        println("Some text")
+        print("More text")
+        print(0)
+    }
+}

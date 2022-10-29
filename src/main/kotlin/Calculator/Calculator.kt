38b49f3c62d0c384d1eee086ecfbebcb0ac0be30
Wout Vinckevleugel <50151315+Wovi10@users.noreply.github.com>
2022-10-29 14:31:56 +1100

    Fixed test issue

--- src/main/kotlin/Calculator/Calculator.kt
+++
@@ -1,8 +0,0 @@
-package Calculator
-
-class Calculator {
-    fun parse(s: String): Int {
-        val (a, op, b) = s.split(" ")
-    }
-
-}
---
+++ src/test/calculator/CalculatorTest.kt
@@ -0,0 +1,20 @@
+package calculator
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class CalculatorTest {
+
+    @Test
+    fun multiply2By2() {
+        val calc = Calculator()
+        val result = calc.parse("2 * 2")
+        assertEquals(4, result)
+    }
+
+    @Test
+    internal fun divide2By2() {
+        TODO("Not yet implemented")
+    }
+}

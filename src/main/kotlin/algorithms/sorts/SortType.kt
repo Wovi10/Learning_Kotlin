e0f01dd143f9ca71345dcb0ba2076a6f04cf8ad9
Wout Vinckevleugel <50151315+Wovi10@users.noreply.github.com>
2022-10-31 19:23:10 +1100

    Shortening if structure

--- src/main/kotlin/algorithms/sorts/SortType.kt
+++ src/main/kotlin/algorithms/sorts/SortType.kt
@@ -20,24 +20,20 @@ abstract class SortType {
 
     open fun printSortInfo(numRun: Int, sortType: SortType) {
         val stopTime: LocalDateTime = LocalDateTime.now()
-        var time = startTime.until(stopTime, ChronoUnit.NANOS).toDouble()
-        val timeString: String
-        if (time >= 1000000000) {
-            time /= 1000000000
+        var time = (startTime.until(stopTime, ChronoUnit.NANOS)/1000000).toDouble()
+        var timeString = "It took $time nanos."
+
+        if (time >= 1000) {
+            time /= 1000
+            timeString = "It took $time seconds."
             if (time >= 60){
                 time /= 60
+                timeString = "It took $time minutes."
                 if (time >= 60){
                     time /= 60
                     timeString = "It took $time hours."
-                }else{
-                    timeString = "It took $time minutes."
                 }
-            }else{
-                timeString = "It took $time seconds."
             }
-        } else {
-            
-            timeString = "It took $time nanos."
         }
         println("Ran ${sortType.name} $numRun times.")
         println(timeString)

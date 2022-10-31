f9a980b5bb1cdd4b75eede2c26cbd788c025939a
Wout Vinckevleugel <50151315+Wovi10@users.noreply.github.com>
2022-10-31 16:23:41 +1100

    Bug found: stackoverflow on running linearsearch

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,12 @@
 import algorithms.searches.BinarySearch
+import algorithms.searches.LinearSearch
 
 fun main(args: Array<String>) {
 //    println("What is your name?")
 //    val name = readln()
 //    println("Hello, $name")
 
-//    BinarySearch.binarySearch(0, 100, 1000)
     BinarySearch.search(0, 100, 1000)
     BinarySearch.search(0, 100, 1000)
-
+    LinearSearch.search(0, 100, 1000)
 }
--- src/main/kotlin/algorithms/searches/SearchType.kt
+++ src/main/kotlin/algorithms/searches/SearchType.kt
@@ -3,19 +3,19 @@ package algorithms.searches
 import kotlin.random.Random
 
 abstract class SearchType {
-    abstract var highestNumTries: Int
-    abstract var numOnHighestTries: Int
-    abstract var lowestNumTries: Int
-    abstract var numOnLowestTries: Int
-    abstract var cumulativeTries: Int
-    abstract var numTries: Int
+    protected abstract var highestNumTries: Int
+    protected abstract var numOnHighestTries: Int
+    protected abstract var lowestNumTries: Int
+    protected abstract var numOnLowestTries: Int
+    protected abstract var cumulativeTries: Int
+    protected abstract var numTries: Int
 
     abstract fun search(lowerBound: Int, upperBound: Int, numRun: Int)
 
     abstract fun findNumber(lowerBound: Int, upperBound: Int, numToFind: Int)
 
     open fun printSearchInfo(numRun: Int){
-        val averageTries = BinarySearch.cumulativeTries / numRun
+        val averageTries = cumulativeTries / numRun
         println("Ran Binary search $numRun times.")
         println("Highest number of tries is $highestNumTries (Number was $numOnHighestTries).")
         println("Lowest number of tries is $lowestNumTries (Number was $numOnLowestTries).")

6baa580a375721d9627d74c510eebab306c349cc
Wout Vinckevleugel <50151315+Wovi10@users.noreply.github.com>
2022-10-31 14:05:51 +1100

    Implemented structure of binary search

--- src/main/kotlin/Algroythms/Algorythms.kt
+++ src/main/kotlin/Algroythms/Algorythms.kt
@@ -2,6 +2,18 @@ package Algroythms
 
 object Algorythms {
     fun binarySearch() {
+        val lowerBound = 0
+        val upperBound = 100
+        val numToFind  = createNumberToFind(lowerBound, upperBound)
+        findNumber(lowerBound, upperBound, numToFind)
+    }
+
+    private fun findNumber(lowerBound: Int, upperBound: Int, numToFind: Int) {
         TODO("Not yet implemented")
     }
+
+    private fun createNumberToFind(lowerBound: Int, upperBound: Int): Int {
+        TODO("Not yet implemented")
+    }
+
 }

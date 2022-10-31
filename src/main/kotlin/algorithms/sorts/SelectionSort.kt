4d34086f68b0b68f8604b2aef79e4ea9e65b762a
Wout Vinckevleugel <50151315+Wovi10@users.noreply.github.com>
2022-10-31 17:53:42 +1100

    Seems to work

--- src/main/kotlin/algorithms/sorts/SelectionSort.kt
+++ src/main/kotlin/algorithms/sorts/SelectionSort.kt
@@ -9,25 +9,31 @@ object SelectionSort: SortType() {
         for (x in 0 until numRun){
             runSelectionSort(size, lowerBound, upperBound)
         }
-
-        TODO("Not yet implemented")
     }
 
     private fun runSelectionSort(size: Int, lowerBound: Int, upperBound: Int) {
         val arrayToSort = createInputArray(size, lowerBound, upperBound)
-        printArray(arrayToSort)
+//        printArray(arrayToSort)
         var smallestIndex: Int
-        for (i in arrayToSort.indices) {
+        for (i in 0 .. arrayToSort.size) {
             smallestIndex = i
             for (j in i until arrayToSort.size) {
-                if (arrayToSort[j] < smallestIndex){
+                if (arrayToSort[j] < arrayToSort[smallestIndex]){
                     smallestIndex = j
                 }
             }
-            arrayToSort[i] xor arrayToSort[smallestIndex]
+            if (smallestIndex != i){
+                swapIndexes(arrayToSort, i, smallestIndex)
+            }
         }
-        printArray(arrayToSort)
-//        TODO("Not yet implemented")
+//        printArray(arrayToSort)
+
+    }
+
+    private fun swapIndexes(arrayToSort: IntArray, i: Int, smallestIndex: Int) {
+        arrayToSort[i] = arrayToSort[i] xor arrayToSort[smallestIndex]
+        arrayToSort[smallestIndex] = arrayToSort[i] xor arrayToSort[smallestIndex]
+        arrayToSort[i] = arrayToSort[i] xor arrayToSort[smallestIndex]
     }
 
     private fun printArray(arrayToSort: IntArray) {

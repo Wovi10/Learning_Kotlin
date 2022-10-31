c0a4cc610e3b93d8bd214992043bd3b5cdb66e86
Wout Vinckevleugel <50151315+Wovi10@users.noreply.github.com>
2022-10-31 15:12:33 +1100

    Output info works

--- src/main/kotlin/algorithms/Algorithms.kt
+++ src/main/kotlin/algorithms/Algorithms.kt
@@ -6,41 +6,41 @@ object Algorithms {
     private var highestNumTries = 1
     private var lowestNumTries = 500
     private var cumulativeTries = 0
+    private var numTries = 0
     fun binarySearch(lowerBound: Int, upperBound: Int, numRun: Int) {
         for (x in 0..numRun) {
             runBinarySearch(lowerBound, upperBound)
         }
         val averageTries = cumulativeTries / numRun
-//        println("Highest number of tries is $highestNumTries")
-//        println("Lowest number of tries is $lowestNumTries")
-//        println("Average number of tries is $averageTries")
+        println("Highest number of tries is $highestNumTries")
+        println("Lowest number of tries is $lowestNumTries")
+        println("Average number of tries is $averageTries")
     }
 
     private fun runBinarySearch(lowerBound: Int, upperBound: Int) {
         val numToFind = createNumToFind(lowerBound, upperBound)
-        var numTries = 0
-        numTries = findNumber(lowerBound, upperBound, numToFind, numTries)
+        numTries = 0
+        findNumber(lowerBound, upperBound, numToFind)
         if (numTries > highestNumTries){
             highestNumTries = numTries
+            println("New highest number $highestNumTries")
         }else if (numTries < lowestNumTries){
             lowestNumTries = numTries
         }
-        println(numTries)
         cumulativeTries += numTries
     }
 
-    private fun findNumber(lowerBound: Int, upperBound: Int, numToFind: Int, _numTries: Int): Int {
-        val numTries = _numTries + 1
+    private fun findNumber(lowerBound: Int, upperBound: Int, numToFind: Int) {
+        numTries++
         val middleNumber = getMiddleNum(lowerBound, upperBound)
         if (numToFind < middleNumber) {
             println("$middleNumber is too high!")
-            return findNumber(lowerBound, middleNumber, numToFind, numTries)
+            return findNumber(lowerBound, middleNumber, numToFind)
         } else if (numToFind > middleNumber) {
             println("$middleNumber is too low!")
-            return findNumber(middleNumber, upperBound, numToFind, numTries)
+            return findNumber(middleNumber, upperBound, numToFind)
         }
         println("Found the number in $numTries times! Number was $numToFind")
-        return middleNumber
     }
 
     private fun getMiddleNum(lowerBound: Int, upperBound: Int): Int {

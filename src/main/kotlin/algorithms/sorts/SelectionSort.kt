d1bf1405d1b67286b164ecfa8be9dedacd01d436
Wout Vinckevleugel <50151315+Wovi10@users.noreply.github.com>
2022-10-31 17:35:14 +1100

    Added selectionSort trying next

--- src/main/kotlin/algorithms/sorts/SelectionSort.kt
+++ src/main/kotlin/algorithms/sorts/SelectionSort.kt
@@ -1,6 +1,9 @@
 package algorithms.sorts
 
+import java.time.LocalDateTime
+
 object SelectionSort: SortType() {
+    override var startTime: LocalDateTime = LocalDateTime.now()
 
     override fun sort(size: Int, lowerBound: Int, upperBound: Int, numRun: Int) {
         for (x in 0 .. numRun){
@@ -12,6 +15,18 @@ object SelectionSort: SortType() {
 
     private fun runSelectionSort(size: Int, lowerBound: Int, upperBound: Int) {
         val arrayToSort = createInputArray(size, lowerBound, upperBound)
+        println(arrayToSort)
+        var smallestIndex: Int
+        for (i in 0 .. arrayToSort.size) {
+            smallestIndex = i
+            for (j in i .. arrayToSort.size) {
+                if (arrayToSort[j] < smallestIndex){
+                    smallestIndex = j
+                }
+            }
+            arrayToSort[i] xor arrayToSort[smallestIndex]
+        }
+        println(arrayToSort)
         TODO("Not yet implemented")
     }
 }
--- src/main/kotlin/algorithms/sorts/SortType.kt
+++ src/main/kotlin/algorithms/sorts/SortType.kt
@@ -1,8 +1,10 @@
 package algorithms.sorts
 
+import java.time.LocalDateTime
 import kotlin.random.Random
 
 abstract class SortType {
+    protected abstract var startTime: LocalDateTime
     abstract fun sort(size: Int, lowerBound: Int, upperBound: Int, numRun: Int)
 
     open fun createInputArray(size: Int, lowerBound: Int, upperBound: Int): IntArray{

ad418ad4c3e78fccbede5f5ab5db3539b5dc9910
Wout Vinckevleugel <50151315+Wovi10@users.noreply.github.com>
2022-11-05 19:01:08 +1100

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,26 +7,30 @@ fun main() {
 //    println("What is your name?")
 //    val name = readln()
 //    println("Hello, $name")
+    val sortList = listOf(QuickSort, RadixSort, SelectionSort, InsertionSort, BubbleSort)
+    val skipSortList = listOf(SelectionSort, InsertionSort)
 
-    val arraySize = 20; val lowestVal = 0; val highestVal = 99; val numRun = 100
+    val searchList = listOf(BinarySearch, LinearSearch)
+    val skipSearchList  = listOf(BinarySearch, LinearSearch)
 
-//    BinarySearch.search(0, 100, 100)
-//    LinearSearch.search(0, 100, 100)
+    val arraySize = 1000000; val lowestVal = 0; val highestVal = 99; val numRun = 100
 
-//    QuickSort.sort(arraySize, lowestVal, highestVal, numRun)
-//    RadixSort.sort(arraySize, lowestVal, highestVal, numRun)
-//    SelectionSort.sort(arraySize, lowestVal, highestVal, numRun)
-//    InsertionSort.sort(arraySize, lowestVal, highestVal, numRun)
+    for (searchType in searchList) {
+        if (skipSearchList.contains(searchType))
+            continue
+        searchType.search(lowestVal, highestVal, numRun)
+    }
 
-//    val quickArrayToSort = Utils.createInputArray(arraySize, lowestVal, highestVal)
-//    QuickSort.sort(quickArrayToSort)
-//    val radixArrayToSort = Utils.createInputArray(arraySize, lowestVal, highestVal)
-//    RadixSort.sort(radixArrayToSort)
-//    val selectionArrayToSort = Utils.createInputArray(arraySize, lowestVal, highestVal)
-//    SelectionSort.sort(selectionArrayToSort)
-//    val insertionArrayToSort = Utils.createInputArray(arraySize, lowestVal, highestVal)
-//    InsertionSort.sort(insertionArrayToSort)
-    val bubbleArrayToSort = Utils.createInputArray(arraySize, lowestVal, highestVal)
-    BubbleSort.sort(bubbleArrayToSort)
+//    for (sortType in sortList) {
+//        if (skipSortList.contains(sortType))
+//            continue
+//        sortType.sort(arraySize, lowestVal, highestVal, numRun)
+//    }
 
+    for (sortType in sortList) {
+        if (skipSortList.contains(sortType))
+            continue
+        val arrayToSort = Utils.createInputArray(arraySize, lowestVal, highestVal)
+        sortType.sort(arrayToSort)
+    }
 }

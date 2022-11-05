23b8dc0750dabc9b86bf37039911cfda3bb9e456
Wout Vinckevleugel <50151315+Wovi10@users.noreply.github.com>
2022-11-05 18:05:17 +1100

    Started insertion sort

--- src/main/kotlin/algorithms/sorts/InsertionSort.kt
+++ src/main/kotlin/algorithms/sorts/InsertionSort.kt
@@ -1,4 +1,38 @@
 package algorithms.sorts
 
-object InsertionSort {
+import algorithms.utils.AlgorithmConstants.LOADING_SYMBOL
+import algorithms.utils.AlgorithmConstants.NEWLINE
+import algorithms.utils.AlgorithmConstants.SORT_TEXT
+import algorithms.utils.Utils
+import java.time.LocalDateTime
+
+object InsertionSort: SortType() {
+    override var startTime: LocalDateTime = LocalDateTime.now()
+    override val name: String = "Insertion $SORT_TEXT"
+
+    override fun sort(arraySize_: Int, lowestValue_: Int, highestValue_: Int, numRun_: Int) {
+        resetVariables()
+        printStartText(numRun_, name)
+        repeat(numRun_){
+            val arrayToSort = Utils.createInputArray(arraySize_, lowestValue_, highestValue_)
+            insertionSort(arrayToSort)
+            print(LOADING_SYMBOL)
+        }
+        print(NEWLINE)
+        printEndText(numRun_, name)
+    }
+
+    override fun sort(arrayToSort_: IntArray) {
+        resetVariables()
+        printStartText(arrayToSort_, name)
+        insertionSort(arrayToSort_)
+        printEndText()
+    }
+
+    private fun insertionSort(arrayToSort_: IntArray) {
+        
+
+
+        TODO("Not yet implemented")
+    }
 }

be2b09a7d6f4af8c14979e8e2de3626899c2f418
Wout Vinckevleugel <50151315+Wovi10@users.noreply.github.com>
2022-11-10 16:13:26 +1100

    ProgressBar works

--- src/main/kotlin/utils/console/AsciiProgressBar.kt
+++ src/main/kotlin/utils/console/AsciiProgressBar.kt
@@ -12,23 +12,34 @@ import utils.console.RichText.RESET
 
 private const val MAX_NUM_HASHTAGS = 25
 
+private const val DONE_MESSAGE = "Done!"
+
 object AsciiProgressBar {
     fun updateProgressBar(index: Int, total: Int){
+        val contentToPrint: String
         val percentage = (index* HUNDRED)/total
         if (percentage + ONE == HUNDRED){
-            print(REVERT + FOREGROUND_GREEN_BRIGHT + "Done!" + RESET)
-            return
+            contentToPrint = FOREGROUND_GREEN_BRIGHT + DONE_MESSAGE + fillBarWithSpaces()
+        }else{
+            val width = (percentage + ONE)/(HUNDRED/ MAX_NUM_HASHTAGS)
+            var hashtags = EMPTY_STRING
+            repeat(width){
+                hashtags += HASHTAG
+            }
+            var spaces = EMPTY_STRING
+            repeat(MAX_NUM_HASHTAGS - width){
+                spaces += SPACE
+            }
+            contentToPrint = "$FOREGROUND_BLUE[$hashtags$spaces]"
         }
-        val width = (percentage + ONE)/(HUNDRED/ MAX_NUM_HASHTAGS)
-        var hashtags = EMPTY_STRING
-        repeat(width){
-            hashtags += HASHTAG
+        print(REVERT + contentToPrint + RESET)
+    }
+
+    private fun fillBarWithSpaces(): String {
+        var output = ""
+        repeat(MAX_NUM_HASHTAGS - DONE_MESSAGE.length){
+            output += SPACE
         }
-        var spaces = EMPTY_STRING
-        repeat(MAX_NUM_HASHTAGS - width){
-            spaces += SPACE
-        }
-        val bar = "[$hashtags$spaces]"
-        print(REVERT + FOREGROUND_BLUE + bar + RESET)
+        return output
     }
 }

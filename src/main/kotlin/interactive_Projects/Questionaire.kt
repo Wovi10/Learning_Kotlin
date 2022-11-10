56e85f27b083e5765765a7fa693b0f65f6ae4bb7
Wout Vinckevleugel <50151315+Wovi10@users.noreply.github.com>
2022-11-10 13:17:25 +1100

    Started setQuestions

--- src/main/kotlin/interactive_Projects/Questionaire.kt
+++ src/main/kotlin/interactive_Projects/Questionaire.kt
@@ -1,11 +1,24 @@
 package interactive_Projects
 
-class Questionaire constructor(numQuestions: Int) {
+class Quiz constructor(numQuestions: Int) {
     private var numQuests: Int
     private var qAndA: ArrayList<Pair<String, String>>
 
     init {
         numQuests = numQuestions
         qAndA = ArrayList<Pair<String, String>>()
+    }
+
+    private fun setQuestions(){
+        repeat(numQuests){questNumber ->
+            println("What is your question?")
+            val question = readln()
+            println("What is the answer?")
+            val answer = readln()
+            val pairToAdd = Pair(question, answer)
+            qAndA.add(pairToAdd)
+        }
+        println("That were all the questions.")
+        println("Press")
     }
 }

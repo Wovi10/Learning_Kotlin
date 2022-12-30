e7dba3e616581be1a738d1865897c444bca3b971
Wout Vinckevleugel <50151315+Wovi10@users.noreply.github.com>
2022-12-30 14:37:59 +1100

    Adding Russian multiplication

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,6 +6,9 @@ import algorithms.sorts.*
 import algorithms.sorts.Sorts.runSorts
 import interactive_Projects.Quiz
 import utils.Constants.EMPTY_STRING
+import utils.Constants.ONE
+import utils.Constants.TWO
+import utils.Constants.ZERO
 
 val sortList = listOf(BogoSort, BubbleSort, InsertionSort, QuickSort, RadixSort, SelectionSort, MergeSort, HeapSort)
 val skipSortList = listOf(EmptySort, BogoSort, QuickSort, RadixSort)
@@ -20,8 +23,32 @@ const val sort_numRun = 1000
 const val search_numRun = 100
 
 fun main() {
-    searchAndSorts()
+    russianPeasant(12,13)
+//    searchAndSorts()
 //    interActiveProjects()
+}
+
+fun russianPeasant(firstNum: Int, multiplier: Int) {
+    val answer = calculate(firstNum, multiplier)
+    printAnswer(answer)
+}
+
+fun printAnswer(answer: Int) {
+    println("The answer is: $answer")
+}
+
+fun calculate(firstNum_: Int, multiplier_: Int): Int {
+    var firstNum = firstNum_
+    var multiplier = multiplier_
+    var sum = ZERO
+    while (firstNum != ONE){
+        firstNum /= TWO
+        multiplier *= TWO
+        if (firstNum % 2 != 0){
+            sum += multiplier
+        }
+    }
+    return sum
 }
 
 fun interActiveProjects() {
--- src/main/kotlin/utils/Constants.kt
+++ src/main/kotlin/utils/Constants.kt
@@ -3,6 +3,7 @@ package utils
 object Constants {
     const val ZERO = 0
     const val ONE = 1
+    const val TWO = 2
 
     const val EMPTY_STRING = ""
     const val SPACE = " "
